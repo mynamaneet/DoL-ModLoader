cc0cf070626066a5ce55cada1d3c14b3c2eaecf2
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-09-22 16:21:25 -0700

    Add hasChanged property to DolLocation

--- src/main/java/com/mynamaneet/dolmodloader/DolLocation.java
+++ src/main/java/com/mynamaneet/dolmodloader/DolLocation.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DolLocation {
-    public DolLocation(File _directoryPath, String _name){
-        this.directoryPath = _directoryPath;
-        this.name = _name;
-    }
     public DolLocation(File _directoryPath, String _name, String _parent){
         this.directoryPath = _directoryPath;
         this.name = _name;
         this.parent = _parent;
+    }
+    public DolLocation(File _directoryPath, String _name, String _parent, boolean _hasChanged){
+        this.directoryPath = _directoryPath;
+        this.name = _name;
+        this.parent = _parent;
+        this.hasChanged = _hasChanged;
     }
 
     private File directoryPath;
     private String name;
     private String parent = "";
     private ArrayList<TweeFile> tweeFiles = new ArrayList<>();
+    private boolean hasChanged = false;
 
 
     public File getDirectoryPath(){
